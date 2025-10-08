272c8959aa251cee5844acebf4a1f026c10bbefd
savagerunderscorelex <eigbekoyi@gmail.com>
2025-10-07 21:01:19 -0400

    added score scene

--- flappy-game/scripts/cave_spikes.gd
+++ flappy-game/scripts/cave_spikes.gd
@@ -6,5 +6,5 @@ func _physics_process(delta: float) -> void:
 	position.x -= speed * delta
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(_body: Node2D) -> void:
 	get_tree().reload_current_scene()
