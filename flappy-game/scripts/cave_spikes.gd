357116c2a8fbc9c5e1588c4a7462a86d36bd7aba
savagerunderscorelex <eigbekoyi@gmail.com>
2025-10-08 18:23:13 -0400

    added main menu + start button, adjusted start scene, added restart when the player touches the edges of the screen,

--- flappy-game/scripts/cave_spikes.gd
+++ flappy-game/scripts/cave_spikes.gd
@@ -8,3 +8,7 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
 	get_tree().reload_current_scene()
+
+
+func _on_score_body_entered(body: Node2D) -> void:
+	Autoload.score += 1
--- flappy-game/scripts/main.gd
+++ flappy-game/scripts/main.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-@onready var spikes : PackedScene = preload("res://screens/cave_spikes.tscn")
+@onready var spikes : PackedScene = preload("res://scenes/cave_spikes.tscn")
+
+func _ready() -> void:
+	Autoload.score = 0
+
+func _process(delta: float) -> void:
+	$CanvasLayer/Control/Label.text = str(Autoload.score)
 
 func _on_timer_timeout() -> void:
 	var spikesInstance = spikes.instantiate()
@@ -8,3 +14,7 @@ func _on_timer_timeout() -> void:
 	spikesInstance.position.y = randi_range(100, 400)
 	add_child(spikesInstance)
 	$Timer.start()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().reload_current_scene()
