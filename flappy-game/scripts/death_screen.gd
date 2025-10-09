1af7a02e4ca8fbd4ef5b8ad956c25909cae7e495
savagerunderscorelex <eigbekoyi@gmail.com>
2025-10-09 18:07:42 -0400

    changed the font of the death screen vars to maybe get rid of the encoding problem, added zip file number 6

--- flappy-game/scripts/death_screen.gd
+++ flappy-game/scripts/death_screen.gd
@@ -4,6 +4,6 @@ func _on_to_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _enter_tree() -> void:
-	$Deathsbyboundaries.text = "deaths by boundaries: %s" %[str(Autoload.deathsByBoundaries)]
-	$Deathsbyspikes.text = "deaths by spikes: %s" %[str(Autoload.deathsBySpikes)]
-	$Score.text = "score: %s" %[str(Autoload.score)]
+	$Deathsbyboundariesvar.text = "%s" %[str(Autoload.deathsByBoundaries)]
+	$Deathsbyspikesvar.text = "%s" %[str(Autoload.deathsBySpikes)]
+	$Scorevar.text = "%s" %[str(Autoload.score)]
