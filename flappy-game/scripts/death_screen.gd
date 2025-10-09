fa6b676a4b07eccdba26916e528084f67b5f9b6a
savagerunderscorelex <eigbekoyi@gmail.com>
2025-10-09 18:18:48 -0400

    most likely final export: the special characters were the ones that didn't work, so I had to use a different font to make them appear and remove any special characters

--- flappy-game/scripts/death_screen.gd
+++ flappy-game/scripts/death_screen.gd
@@ -4,6 +4,6 @@ func _on_to_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 func _enter_tree() -> void:
-	$Deathsbyboundariesvar.text = "%s" %[str(Autoload.deathsByBoundaries)]
-	$Deathsbyspikesvar.text = "%s" %[str(Autoload.deathsBySpikes)]
-	$Scorevar.text = "%s" %[str(Autoload.score)]
+	$Deathsbyboundariesvar.text = ": %s" %[str(Autoload.deathsByBoundaries)]
+	$Deathsbyspikesvar.text = ": %s" %[str(Autoload.deathsBySpikes)]
+	$Scorevar.text = ": %s" %[str(Autoload.score)]
